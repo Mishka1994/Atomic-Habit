7c55763ad047594fdbd4e483b0411df368567739
Mihail Samofalov <misasamofalov611@gmail.com>
2024-02-11 19:55:24 +0700

    create Place model

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users'))
+    path('users/', include('users.urls', namespace='users')),
+    path('habit/', include('habit.urls', namespace='habit')),
+
 ]
--- habit/models.py
+++ habit/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name_of_place = models.CharField(max_length=150, verbose_name='Название места')
+    comments = models.TextField(verbose_name='Комментарии')
+
+    def __str__(self):
+        return f'{self.name_of_place}'
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
